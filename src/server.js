62766cd93ce9faecd13e3af2005f87ecd4b98d87
Volodymyr <110925314+woland31252@users.noreply.github.com>
2024-07-17 17:39:45 +0300

    fix patch/contacts/id

--- src/server.js
+++ src/server.js
@@ -9,6 +9,7 @@ import router from '../src/routers/index.js';
 import errorHandler from './middlewares/errorHandler.js';
 import notFoundHandler from './middlewares/notFoundHandler.js';
 import { UPLOAD_DIR } from './constants/index.js';
+import swaggerDocs from './middlewares/swaggerDocs.js';
 
 const PORT = Number(env('PORT', '3000'));
 
@@ -49,6 +50,7 @@ export const setupServer = () => {
   });
 
   app.use('/uploads', express.static(UPLOAD_DIR));
+  app.use('/api-docs', swaggerDocs());
 };
 
 

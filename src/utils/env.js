5c06602389e66c04b971b2ece658c8618f0f41a0
Volodymyr <110925314+woland31252@users.noreply.github.com>
2024-07-19 16:59:31 +0300

    fix auth swagger

--- src/utils/env.js
+++ src/utils/env.js
@@ -3,9 +3,16 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-export function env(name, defaultvalue) {
+function env(name, defaultvalue) {
   const value = process.env[name];
   if (value) return value;
   if (defaultvalue) return defaultvalue;
   throw new Error(`Missing: process.env['${name}'].`);
 }
+
+const ENV_VARS = {
+  GOOGLE_AUTH_CLIENT_ID: 'GOOGLE_AUTH_CLIENT_ID',
+  GOOGLE_AUTH_CLIENT_SECRET: 'GOOGLE_AUTH_CLIENT_SECRET',
+};
+
+export { env, ENV_VARS };
--- src/utils/googleOAuth.js
+++ src/utils/googleOAuth.js
@@ -4,15 +4,15 @@ import { OAuth2Client } from 'google-auth-library';
 import path from 'node:path';
 import { readFile } from 'fs/promises';
 import createHttpError from 'http-errors';
-import { env } from './env.js';
+import { env, ENV_VARS } from './env.js';
 
 const PATH_JSON = path.join(process.cwd(), 'google-oauth.json');
 
 const oauthConfig = JSON.parse(await readFile(PATH_JSON));
 
 const googleOAuthClient = new OAuth2Client({
-  clientId: env('GOOGLE_AUTH_CLIENT_ID'),
-  clientSecret: env('GOOGLE_AUTH_CLIENT_SECRET'),
+  clientId: env(ENV_VARS.GOOGLE_AUTH_CLIENT_ID),
+  clientSecret: env(ENV_VARS.GOOGLE_AUTH_CLIENT_SECRET),
   redirectUri: oauthConfig.web.redirect_uris[0],
 });
 

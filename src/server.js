6d9e546dc41b87f7e18376e8b6beb6ee58df8a80
Volodymyr <110925314+woland31252@users.noreply.github.com>
2024-07-19 15:29:57 +0300

    create swagger auth

--- src/server.js
+++ src/server.js
@@ -14,17 +14,17 @@ import swaggerDocs from './middlewares/swaggerDocs.js';
 const PORT = Number(env('PORT', '3000'));
 
 export const setupServer = () => {
-    const app = express();
-
-  app.use(cors());
-  app.use(cookieParser());
+  const app = express();
   app.use(
     express.json({
       type: ['application/json', 'application/vnd.api+json'],
       limit: '100kb',
     }),
-);
-
+  );
+  app.use('/uploads', express.static(UPLOAD_DIR));
+  app.use('/api-docs', swaggerDocs());
+  app.use(cors());
+  app.use(cookieParser());
   app.use(
     pino({
       transport: {
@@ -45,12 +45,11 @@ export const setupServer = () => {
 
   app.use(errorHandler);
 
+  // app.use('/uploads', express.static(UPLOAD_DIR));
+
   app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
   });
-
-  app.use('/uploads', express.static(UPLOAD_DIR));
-  app.use('/api-docs', swaggerDocs());
 };
 
 
